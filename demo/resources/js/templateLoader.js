302b16cc78d0663a9e4950cada14731a7acd8038
ruisuiwnl <e.luis.pelaez@gmail.com>
2025-12-12 22:59:00 +0800

    update to run script immediately when template loads

--- demo/resources/js/templateLoader.js
+++ demo/resources/js/templateLoader.js
@@ -41,6 +41,16 @@ class TemplateLoader {
             targetElement.innerHTML = html;
         }
 
+        const scripts = targetElement.querySelectorAll('script');
+        scripts.forEach(oldScript => {
+            const newScript = document.createElement('script');
+            Array.from(oldScript.attributes).forEach(attr => {
+                newScript.setAttribute(attr.name, attr.value);
+            });
+            newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+            oldScript.parentNode.replaceChild(newScript, oldScript);
+        });
+
         return targetElement;
     }
 
